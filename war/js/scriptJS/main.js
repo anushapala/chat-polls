26559bc3f5dcc1dfa73860147b4f918f1f932f8f
John Britto <john.britto@a-cti.com>
2016-10-15 20:16:15 +0530

    Syncec the changes.

--- war/js/scriptJS/main.js
+++ war/js/scriptJS/main.js
@@ -1,4 +1,4 @@
-var app = AAFClient.init();
+var app = null;
 
 
 $(document).ready(function(){
@@ -27,25 +27,35 @@ $(document).ready(function(){
 	$('#create-poll').on('click',function(e){
 		var pollQuestion = $('#poll-question').find('textarea').val();
 		var pollDescription = $('#poll-description').find('textarea').val();
-		
-		var pollOptionsArrayList = [];
-		
-		$('#poll-option-container').find('div.poll-option').each(function(){
-			var pollOptionMap = {};
-			var pollImg = $(this).find('img').attr('src');
-			var pollOption =  $('#poll-option-container').find('div.poll-option:eq(0)').find('input.option-holder').val();
-			pollOptionMap = {
-					'pollOptionImageURL' : pollImg,
-					'pollOptionContent' : pollOption
-			}
-			pollOptionsArrayList.push(pollOptionMap);
 			
+		var empty = $('#poll-option-container').find('input.option-holder').filter(function() {
+			return this.value === "";
 		});
 		
-		console.log(pollQuestion+"\n"+pollDescription+"\n"+pollOptionsArrayList);
-		
-		PollOperations.createPoll(pollQuestion,pollDescription,pollOptionsArrayList);
-		
+		if(empty.length) {
+			$(".Polling").animate({marginLeft: "+=25px"},100);
+			$(".Polling").animate({marginLeft: "-=50px"},100);
+			$(".Polling").animate({marginLeft: "+=25px"},100);
+		}else{
+			var pollOptionsArrayList = [];
+			
+			$('#poll-option-container').find('div.poll-option').each(function(){
+				var pollOptionMap = {};
+				var pollImg = $(this).find('img').attr('src');
+				var pollOption = $(this).find('input.option-holder').val();
+				pollOptionMap = {
+				'pollOptionImageURL' : pollImg,
+				'pollOptionContent' : pollOption
+				}
+				pollOptionsArrayList.push(pollOptionMap);
+				
+			});	
+			
+			console.log(pollQuestion+"\n"+pollDescription+"\n"+pollOptionsArrayList);
+
+			PollOperations.createPoll(pollQuestion,pollDescription,pollOptionsArrayList);
+		}
+			
 	});
 	
 });
@@ -109,6 +119,8 @@ function showImage(id, fileInput) {
 
 
 var Poll = (function($,window,document,undefined){
+	
+	
 	var _appuser, _context = {};
 	
 	var getAppUser = function(){
@@ -118,7 +130,7 @@ var Poll = (function($,window,document,undefined){
 		return _context;
 	};
 	var _init = function(){
-		
+		app = AAFClient.init();
 		app.on('registered', function(data) {
 			   console.error("On app.registered event.",data);
 			   _appUser = data.user;
@@ -139,7 +151,8 @@ var Poll = (function($,window,document,undefined){
 			});
 			
 	};
-		
+	_init();
+	
 	return{ 
 		_init : _init,
 		getAppUser : getAppUser,
@@ -152,11 +165,18 @@ var PollOperations = (function($,window,document,undefined){
 	
 	var createPoll = function(pollQuestion,pollDescription,pollOptionsList){
 		
+		if(pollQuestion == "" || pollOptionsList.length == 0 ){
+			console.error('no proper parameters to create the poll');
+			return;
+			
+		}
+		
 		var requestMap = {
 				'pollQuestionDetails' : {
-					//'streamID' : Poll().getContext().id,
-					'streamID' :'3aab167e-fcb5-4b6a-a962-17c48551c204',
-					'pollQuestion' : pollQuestion,
+					//'streamID' : Poll.getContext().id,
+					'streamID' 		  :'3aab167e-fcb5-4b6a-a962-17c48551c204',
+					'createdBy' 	  : '5f3e80ff-e730-470f-a708-bb4639a55a6c',
+					'pollQuestion' 	  : pollQuestion,
 					'pollDescription' : pollDescription
 				},
 				'pollOptionsDetails' : pollOptionsList
@@ -170,31 +190,101 @@ var PollOperations = (function($,window,document,undefined){
 			data : requestJSON,
 			dataType : 'json',
 			contentType:'application/json',
-			success : function(responseJSON){
-				console.log(responseJSON);
-				var data = JSON.parse(responseJSON);
-				console.log(data);
-				
-			},failure : function(){
-				
+			success : function(response){
+				console.log(response);
+				var pollArray = [];
+				pollArray.push(response);
+				PollOperations.diplayPollsList(pollArray);
+			},failure : function(errResp){
+				console.error(errResp);
 			}
 		});
 		
 	};
 	
-	var fillPollInfo = function(){
-		
+	var fetchPollsForTheStream = function(streamID){
+		if(streamID == ""){
+			console.error("streamID is empty so cant proceed further");
+			return;
+		}
+		var requestMap ={
+				'streamID' : streamID
+		}
+		var requestJSON = JSON.stringify(requestMap);
+		$.ajax({
+			type :'POST',
+			url  :'/poll/fetchPoll',
+			data : requestJSON,
+			dataType : 'json',
+			contentType:'application/json',
+			success: function(response){
+				console.log(response);
+				PollOperations.diplayPollsList(response.PollsDetailsList);
+			},failure :function(errResp){
+				console.error(errResp);
+			}
+		});
 	};
-	var fetchPoll = function(){
+	
+	var diplayPollsList = function(pollsList){
+		
+		$('#polls-list').html("");
+		
+		for(var index in pollsList){
+			
+			var pollQuestionDetails = pollsList[index].pollQuestionDetails;
+			var pollOptionsList = pollsList[index].pollOptionsList;
+			
+			var pollItemsList = "";
+			var count = 0;
+			for(var ind in pollOptionsList){
+				count = count+1;
+				var pollItem = '<div id="'+pollOptionsList[ind].pollOptionID+'">';
+				pollItem += '		<label class="input_label">option '+count+':</label>';
+				pollItem += '		<div class="poll-option">';
+				pollItem += '			<img class="icon" src="'+pollOptionsList[ind].pollOptionImageURL+'">';
+				pollItem += ' 			<input class="input_default input_small option-holder" type="text" readonly value="'+pollOptionsList[ind].pollOptionText+'"/>';
+				pollItem += '		</div>';
+				if(pollOptionsList[ind].optionLikedList != null ){
+					pollItem += '		<code>'+pollOptionsList[ind].optionLikedList.length+' votes</code>';
+				}else{
+					pollItem += '		<code> 0 votes</code>';
+				}
+				pollItem += '	</div>';
+				pollItemsList += pollItem;
+			}
+			
+			var pollDomItem = '<li class="question">';
+			pollDomItem += '		<span class="notification"></span>';
+			pollDomItem += '		<h5 class="name">anusha</h5>';
+			pollDomItem += '		<p>'+pollQuestionDetails.pollQuestion+'</p>';
+			pollDomItem += '		<div class="contract">';
+			pollDomItem += '			<cite>'+pollQuestionDetails.pollDescription+'</cite>';
+			pollDomItem += '			<div calss="option-container">';
+			pollDomItem += 					pollItemsList;		
+			pollDomItem += '			</div>';
+			pollDomItem += '		</div>';
+			pollDomItem += '	</li>';
+			
+			$('#polls-list').append(pollDomItem);
+		}
+		
+		$('#create-poll-container').hide();
+		$('#polls-list-container').show();
 		
 	};
 	
-	var updatePoll
+	var updatePoll = function(){
+		
+		
+		
+		
+	};
 	
 	return{
 		createPoll : createPoll,
-		fetchPoll : fetchPoll,
-		fillPollInfo : fillPollInfo,
+		fetchPollsForTheStream : fetchPollsForTheStream,
+		diplayPollsList : diplayPollsList,
 	}
 	
 })(jQuery,window,document);
