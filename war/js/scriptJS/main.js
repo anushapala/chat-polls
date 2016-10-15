7a831997f5fede57d0c3bb8ab12bcdd2b4fbaf35
HarshaReddy1451 <harsha.vardhan@a-cti.com>
2016-10-15 11:22:17 +0530

    Commit : Image upload in front end.

--- war/js/scriptJS/main.js
+++ war/js/scriptJS/main.js
@@ -1,3 +1,33 @@
 /**
  * 
  */
+$(document).ready(function(){
+	$(".picture-upload").on('click', function () {
+        var id = $(this).attr('id');
+        var number = id.substring(3);
+        $("#imgupload" + number)[0].click();
+    });
+	$("input:file").on('change', function () {
+        var id = $(this).attr('id');
+        var number = id.substring(9);
+        showImage(number, this);
+    });
+});
+function showImage(id, fileInput) {
+    var file = fileInput.files[0];
+    var imageType = /image.*/;
+    if (file.type.match(imageType)) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $("#img" + id).attr("src", reader.result);
+        }
+        reader.onloadend = function (e) {
+        }
+        reader.onerror = function(e)
+        {
+        }
+        reader.readAsDataURL(file);
+    } else {
+        console.log('File not supported' + imageType);
+    }
+}
