e3a771b7b01c7d86edaed18e3468cf99d839a8f9
anusha <anusha.kumar@a-cti.com>
2016-10-15 11:33:32 +0530

    integrated changes from harsha changes

--- war/js/scriptJS/main.js
+++ war/js/scriptJS/main.js
@@ -2,6 +2,16 @@ var app = AAFClient.init();
 
 
 $(document).ready(function(){
+$(".picture-upload").on('click', function () {
+        var id = $(this).attr('id');
+        var number = id.substring(3);
+        $("#imgupload" + number)[0].click();
+    });
+	$("input:file").on('change', function () {
+        var id = $(this).attr('id');
+        var number = id.substring(9);
+        showImage(number, this);
+    });
 	
 	//adding extra option on creating the poll
 	$('#add-option').on('click',function(e){
@@ -23,6 +33,24 @@ $(document).ready(function(){
 	
 });
 
+function showImage(id, fileInput) {
+    var file = fileInput.files[0];
+    var imageType = /image.*/;
+    if (file.type.match(imageType)) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $("#img" + id).attr("src", reader.result);
+        }
+        reader.onloadend = function (e) {
+        }
+        reader.onerror = function(e)
+        {
+        }
+        reader.readAsDataURL(file);
+    } else {
+        console.log('File not supported' + imageType);
+    }
+}
 
 ////default events
 //
