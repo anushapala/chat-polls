cc2ac36cca922a8c4e6c3e59f833418839c7a2f9
anusha <anusha.kumar@a-cti.com>
2016-10-15 14:21:20 +0530

    changes on create poll, delete poll option

--- war/js/scriptJS/main.js
+++ war/js/scriptJS/main.js
@@ -2,16 +2,7 @@ var app = AAFClient.init();
 
 
 $(document).ready(function(){
-$(".picture-upload").on('click', function () {
-        var id = $(this).attr('id');
-        var number = id.substring(3);
-        $("#imgupload" + number)[0].click();
-    });
-	$("input:file").on('change', function () {
-        var id = $(this).attr('id');
-        var number = id.substring(9);
-        showImage(number, this);
-    });
+
 	
 	//adding extra option on creating the poll
 	$('#add-option').on('click',function(e){
@@ -22,8 +13,10 @@ $(".picture-upload").on('click', function () {
 	    var domElement = '<div>';
 	    domElement += '		<label class="input_label">'+label+':</label>';
 	    domElement += '		<div class="poll-option">';
-	    domElement += '			<img class="icon picture-upload" data-imgId= img'+optionsCount+'>';
+	    domElement += '			<img class="icon picture-upload" id= img'+optionsCount+'>';
+	    domElement += '			<input type="file" style="visibility: hidden;display:none" id=imgupload'+optionsCount+'>';
 	    domElement += '         <input class="input_default input_small option-holder" type="text" placeholder="Enter option here...">';
+	    domElement += '	 		<span class="delete"></span>';
 	    domElement += '		</div>';
 	    domElement += '	  </div>';		
 	    
@@ -31,6 +24,47 @@ $(".picture-upload").on('click', function () {
 	});
 	
 	
+	$('#create-poll').on('click',function(e){
+		var pollQuestion = $('#poll-question').find('textarea').val();
+		var pollDescription = $('#poll-description').find('textarea').val();
+		
+		var pollOptionsArrayList = [];
+		
+		$('#poll-option-container').find('div.poll-option').each(function(){
+			var pollOptionMap = {};
+			var pollImg = $(this).find('img').attr('src');
+			var pollOption =  $('#poll-option-container').find('div.poll-option:eq(0)').find('input.option-holder').val();
+			pollOptionMap = {
+					'pollOptionImageURL' : pollImg,
+					'pollOptionContent' : pollOption
+			}
+			pollOptionsArrayList.push(pollOptionMap);
+			
+		});
+		
+		console.log(pollQuestion+"\n"+pollDescription+"\n"+pollOptionsArrayList);
+		
+		PollOperations.createPoll(pollQuestion,pollDescription,pollOptionsArrayList);
+		
+	});
+	
+});
+
+
+$(document.body).on('click','.picture-upload', function () {
+    var id = $(this).attr('id');
+    var number = id.substring(3);
+    $("#imgupload" + number)[0].click();
+});
+
+$(document.body).on('change','input:file', function () {
+    var id = $(this).attr('id');
+    var number = id.substring(9);
+    showImage(number, this);
+});
+
+$(document.body).on('click','.delete',function(){
+	$(this).parent('div.poll-option').parent('div').remove();
 });
 
 function showImage(id, fileInput) {
@@ -116,25 +150,30 @@ var Poll = (function($,window,document,undefined){
 
 var PollOperations = (function($,window,document,undefined){
 	
-	var createPoll = function(pollQuestion,pollDescription,pollOptionList){
+	var createPoll = function(pollQuestion,pollDescription,pollOptionsList){
 		
 		var requestMap = {
-				'pollQuestion' : {
-					'streamId' : Poll().getContext().id,
+				'pollQuestionDetails' : {
+					//'streamID' : Poll().getContext().id,
+					'streamID' :'3aab167e-fcb5-4b6a-a962-17c48551c204',
 					'pollQuestion' : pollQuestion,
 					'pollDescription' : pollDescription
 				},
-				'pollOptions' : pollOptionList
+				'pollOptionsDetails' : pollOptionsList
 		}
 		
 		var requestJSON = JSON.stringify(requestMap);
 		
 		$.ajax({
 			type :'POST',
-			url  :'/poll/createAPoll',
+			url  :'/poll/createPoll',
 			data : requestJSON,
+			dataType : 'json',
+			contentType:'application/json',
 			success : function(responseJSON){
+				console.log(responseJSON);
 				var data = JSON.parse(responseJSON);
+				console.log(data);
 				
 			},failure : function(){
 				
@@ -143,6 +182,9 @@ var PollOperations = (function($,window,document,undefined){
 		
 	};
 	
+	var fillPollInfo = function(){
+		
+	};
 	var fetchPoll = function(){
 		
 	};
@@ -152,6 +194,7 @@ var PollOperations = (function($,window,document,undefined){
 	return{
 		createPoll : createPoll,
 		fetchPoll : fetchPoll,
+		fillPollInfo : fillPollInfo,
 	}
 	
 })(jQuery,window,document);
